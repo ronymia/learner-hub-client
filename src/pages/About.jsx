6556a932dc842c02335d4acf19d062c651ac5ddd
MD Rony Mia <mdronymia.javascript@gmail.com>
2023-02-18 13:13:38 +0600

    about summary

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { PageHeader } from '../components';
+import AboutSummary from '../components/AboutSummary';
 
 const About = () => {
     return (
         <section className="">
             <PageHeader />
+            <AboutSummary />
 
         </section>
     )
