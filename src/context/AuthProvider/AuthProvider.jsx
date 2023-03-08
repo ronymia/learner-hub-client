a1710e3f8f5e426c87967c61ebf90a5bb5bb7ed8
MD Rony Mia <mdronymia.javascript@gmail.com>
2023-03-08 15:26:53 +0600

    auth provider

--- src/context/AuthProvider/AuthProvider.jsx
+++ src/context/AuthProvider/AuthProvider.jsx
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { createContext } from 'react';
+import { auth } from '../../utility/firebase.config';
 
 
-const AuthProvider = () => {
+
+//creating AuthProvider Context
+export const AuthContext = createContext();
+
+const AuthProvider = ({ children }) => {
+     const [user, setUser] = useState(null);
+     const [loading, setLoading] = useState(true);
+
+     //create new user 
+     const createUser = (email, password) => {
+          setLoading(true);
+          return createUserWithEmailAndPassword(auth, email, password);
+     }
+
+
+     // auth information
+     const authInfo = {
+          // user,
+          // loading,
+          // providerLogin,
+          createUser,
+          // signIn,
+          // updateUserProfile,
+          // logOut
+     };
+
      return (
-          <div>
-
-          </div>
+          <AuthContext.Provider value={authInfo} >
+               {children}
+          </AuthContext.Provider>
      );
 }
 
