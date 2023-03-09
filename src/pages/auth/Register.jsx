55e481fc1ab988f9481dee00af7be9af76de71dd
MD Rony Mia <mdronymia.javascript@gmail.com>
2023-03-09 12:34:17 +0600

    signup error

--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useForm } from "react-hook-form";
 
@@ -8,9 +8,10 @@ import { toast } from 'react-hot-toast';
 
 const Register = () => {
     const navigate = useNavigate();
-    const { register, handleSubmit, watch, reset, formState: { errors } } = useForm();
     //auth context 
     const { createUser } = useAuth();
+    const [loginError, setLoginError] = useState('');
+    const { register, handleSubmit, watch, reset, formState: { errors } } = useForm();
 
     //form event handler
     const onSubmit = data => {
@@ -25,7 +26,27 @@ const Register = () => {
                     toast.success('Successfully create Account');
                     navigate("/");
                 }
-            }).catch(error => console.log(error));
+            }).catch(error => {
+                const errorCode = error.code;
+
+                if (errorCode) {
+                    switch (errorCode) {
+                        case 'auth/user-not-found':
+                            setLoginError('User not founded');
+                            break;
+                        case 'auth/invalid-email':
+                            setLoginError('Invalid email provided, please provide a valid email')
+                            break;
+
+                        case 'auth/wrong-password':
+                            setLoginError('Wrong password');
+                            break;
+
+                        default:
+                            setLoginError('Something is wrong');
+                    }
+                }
+            });
     }
 
     // console.log(watch("email"));
@@ -43,9 +64,22 @@ const Register = () => {
                             className="outline-none w-[360px] md:w-[460px] h-14 borderBox py-3 px-4 placeholder:capitalize placeholder:text-lightColor"
                             placeholder="your email"
                             {...register("email", {
-                                required: "Please enter your email."
+                                required: {
+                                    value: true,
+                                    message: "Your Email is required"
+                                },
+                                pattern: {
+                                    value: /^\S+@\S+\.\S+$/,
+                                    message: "Provide a valid Email"
+                                }
                             })}
                         />
+                        {/* error message  */}
+                        <label className="label">
+                            {errors.email?.type === 'required' && <span className="label-text-alt text-red-500">{errors.email.message}</span>}
+                            {errors.email?.type === 'pattern' && <span className="label-text-alt text-red-500">{errors.email.message}</span>}
+                            {loginError && <span className="label-text-alt text-red-500">{loginError}</span>}
+                        </label>
                     </div>
                     {/* password  */}
                     <div className="flex flex-col items-start gap-1 mt-5 mb-2">
@@ -54,9 +88,22 @@ const Register = () => {
                             className="outline-none w-[360px] md:w-[460px] h-14 borderBox py-3 px-4 placeholder:capitalize placeholder:text-lightColor"
                             placeholder="new password"
                             {...register("password", {
-                                required: "Please enter new password."
+                                required: {
+                                    value: true,
+                                    message: "Your Password is required"
+                                },
+                                minLength: {
+                                    value: 6,
+                                    message: "Must be 6 character or longer"
+                                }
                             })}
                         />
+                        {/* error message   */}
+                        <label className="label">
+                            {errors.password?.type === 'required' && <span className="label-text-alt text-red-500">{errors.password.message}</span>}
+                            {errors.password?.type === 'minLength' && <span className="label-text-alt text-red-500">{errors.password.message}</span>}
+                            {loginError && <span className="label-text-alt text-red-500">{loginError}</span>}
+                        </label>
                     </div>
                     {/* confirm password  */}
                     <div className="flex flex-col items-start gap-1 mt-5 mb-2">
@@ -65,9 +112,22 @@ const Register = () => {
                             className="outline-none w-[360px] md:w-[460px] h-14 borderBox py-3 px-4 placeholder:capitalize placeholder:text-lightColor"
                             placeholder="confirm password"
                             {...register("ConfirmPassword", {
-                                required: "please type same password."
+                                required: {
+                                    value: true,
+                                    message: "Your Password is required"
+                                },
+                                minLength: {
+                                    value: 6,
+                                    message: "Must be 6 character or longer"
+                                }
                             })}
                         />
+                        {/* error message   */}
+                        <label className="label">
+                            {errors.ConfirmPassword?.type === 'required' && <span className="label-text-alt text-red-500">{errors.ConfirmPassword.message}</span>}
+                            {errors.ConfirmPassword?.type === 'minLength' && <span className="label-text-alt text-red-500">{errors.ConfirmPassword.message}</span>}
+                            {loginError && <span className="label-text-alt text-red-500">{loginError}</span>}
+                        </label>
                     </div>
                     {/* checkBox  */}
                     <div className="flex flex-row items-center gap-2 capitalize">
