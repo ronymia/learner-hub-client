18c462de7cb264d14d95aadd5ad571b3ac6ec370
MD Rony Mia <mdronymia.javascript@gmail.com>
2023-11-02 00:23:05 +0600

    axiosn implement

--- src/pages/Home/Instructors/Instructors.jsx
+++ src/pages/Home/Instructors/Instructors.jsx
@@ -1,28 +1,25 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 
 
 export default function Instructors() {
-     const [instructors, setInstructors] = useState([]);
-     useEffect(() => {
-          axios.get('instructor.json')
-               .then(res => {
-                    const data = res.data;
-                    setInstructors(data);
-               })
-               .catch(err => console.error(err));
-     }, []);
 
-     console.log(instructors);
+     const { data: instructors } = useQuery({
+          queryKey: ["expertInstutors"],
+          queryFn: async () => {
+               const { data } = await axios.get('instructor.json');
+               return data;
+          }
+     });
 
      return (
           <section className="py-[30px] lg:px-[5%]">
-               <h1 className='text-[35px] capitalize text-dimBlack mb-[30px] block text-center'>
-                    our Instructors
+               <h1 className='text-4xl text-[#413960] uppercase font-bold mb-[30px] block text-center'>
+                    our Expert Instructors
                </h1>
                <div className="grid grid-cols-3 gap-x-6 gap-y-8">
                     {
-                         instructors.map(instructor =>
+                         instructors?.map(instructor =>
                               <div key={instructor._id}
                                    className=""
                               >
