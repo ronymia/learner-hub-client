0369609d3bcaa56020ae1598e943018bb0336f29
MD Rony Mia <mdronymia.javascript@gmail.com>
2023-11-06 22:17:29 +0600

    banner mobile respobsive

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -3,11 +3,11 @@ import bannerImg from '../../../assets/images/banner.png';
 
 export default function Banner() {
      return (
-          <section className="bg-[#F0FFF0] h-[70vh] grid grid-cols-2 px-6 md:px-28 gap-x-6 items-center justify-start">
+          <section className="bg-[#F0FFF0] h-[75vh] md:h-[70vh] grid grid-cols-1 md:grid-cols-2 px-6 md:px-28 gap-y-10 md:gap-x-6 items-center justify-start">
                {/* Left side  */}
-               <div className="w-3/4 flex flex-col items-start justify-start gap-y-5">
+               <div className="w-full md:w-3/4 flex flex-col items-start justify-start gap-y-2 md:gap-y-5 md:order-1 order-2">
                     <h6 className="text-[#FD511A]">Are you ready to Learn?</h6>
-                    <h3 className='text-5xl text-black font-bold leading-snug'>
+                    <h3 className='text-4xl md:text-5xl text-black font-bold leading-snug'>
                          Learn With fun <br />
                          <span className='text-[#21B573]'>on any schedule</span>
                     </h3>
@@ -21,7 +21,7 @@ export default function Banner() {
 
                {/* right side  */}
 
-               <div className="">
+               <div className="md:order-2 order-1">
                     <img src={bannerImg} alt="" />
                </div>
           </section>
